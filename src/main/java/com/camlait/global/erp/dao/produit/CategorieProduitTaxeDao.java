b7509bb22ff5f95a3871ac4dc89353bcbc8bfb82
martin <a-msigne@expedia.com>
2015-12-29 14:25:56 -0500

    Ajout de la collection de taxe pour une categorie de produit

---
+++ src/main/java/com/camlait/global/erp/dao/produit/CategorieProduitTaxeDao.java
@@ -0,0 +1,9 @@
+package com.camlait.global.erp.dao.produit;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.camlait.global.erp.domain.produit.CategorieProduitTaxe;
+
+public interface CategorieProduitTaxeDao extends JpaRepository<CategorieProduitTaxe, Long> {
+
+}
