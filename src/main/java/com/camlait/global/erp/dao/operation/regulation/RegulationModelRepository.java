9102034c686de8031a8a0174c99ed310d4e90cad
mbsig <mbsig@mbossi>
2017-03-20 12:59:28 -0400

    code refatoring

---
+++ src/main/java/com/camlait/global/erp/dao/operation/regulation/RegulationModelRepository.java
@@ -0,0 +1,9 @@
+package com.camlait.global.erp.dao.operation.regulation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.camlait.global.erp.domain.operation.regulation.RegulationModel;
+
+public interface RegulationModelRepository extends JpaRepository<RegulationModel, String> {
+
+}
