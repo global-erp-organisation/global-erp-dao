7df5ef6df7b9475d8b54ce2d8fc1f83eb5ac03ce
mbsig <mbsig@192.168.2.18>
2017-03-29 14:46:01 -0400

    code refactoring

--- src/main/java/com/camlait/global/erp/dao/localisation/LocalisationRepository.java
+++ src/main/java/com/camlait/global/erp/dao/localisation/LocalisationRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.camlait.global.erp.domain.enumeration.EnumTypeEntitity;
-import com.camlait.global.erp.domain.localisation.Localisation;
+import com.camlait.global.erp.domain.localization.Localization;
 
-public interface LocalisationRepository extends JpaRepository<Localisation, String>{
+public interface LocalisationRepository extends JpaRepository<Localization, String>{
     
-    @Query(value = "Select l From Localisation l where l.typeLocal=?1 order by l.code desc")
-    Page<Localisation> obtenirDernierLocal(EnumTypeEntitity type,Pageable p);
+    @Query(value = "Select l From Localization l where l.typeLocal=?1 order by l.code desc")
+    Page<Localization> obtenirDernierLocal(EnumTypeEntitity type,Pageable p);
     
-    Page<Localisation> findByCode(String code,Pageable p);
+    Page<Localization> findByCode(String code,Pageable p);
 }
--- src/main/java/com/camlait/global/erp/dao/tarif/TarificationRepository.java
+++ src/main/java/com/camlait/global/erp/dao/tarif/TarificationRepository.java
@@ -5,15 +5,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.camlait.global.erp.domain.tarif.Tarification;
+import com.camlait.global.erp.domain.tarif.Tariffication;
 
-public interface TarificationRepository extends JpaRepository<Tarification, String> {
+public interface TarificationRepository extends JpaRepository<Tariffication, String> {
 
-    @Query(value = "from Tarification t where t.tarif.tarifId=?1 and t.zone.localId=?2 and t.produit.produitId=?3")
-    Double retrieveUnitPrice(String tarifId, String zoneId, String productId);
+    @Query(value = "from Tariffication t where t.tarif.tarifId=?1 and t.zone.localId=?2 and t.produit.produitId=?3 and t.priceType.priceTypeId=?4")
+    Double retrieveUnitPrice(String tarifId, String zoneId, String productId, String tariffId);
 
-    @Query(value = "from Tarification t where t.tarificationId like %?1% or t.zone.localId like %?1% "
+    @Query(value = "from Tariffication t where t.tarificationId like %?1% or t.zone.localId like %?1% "
             + "or t.zone.code like %?1% or t.produit.produitId like %?1% or t.produit.codeProduit like %?1% "
             + "or t.produit.descriptionProduit like %?1% or t.tarif.tarifId like %?1% or t.tarif.descriptionTarif like %?1%")
-    Page<Tarification> retrieveTarifications(String keyWord, Pageable p);
+    Page<Tariffication> retrieveTarifications(String keyWord, Pageable p);
 }
